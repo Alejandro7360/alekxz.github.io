a807d43456cf1a7bbd53b6315d06f46cfab42ec0
Unknown <ichigo_11_619@live.com.mx>
2020-05-06 23:44:33 -0500

    Files uploaded
    All files uploaded to the repo for remote work

---
+++ EvidenciaDiseño/MusicScript.js
@@ -0,0 +1,152 @@
+function playAudio1(){
+        var audio1 = document.getElementsByTagName("audio")[0];
+        audio1.play();
+        }
+function stopAudio1(){
+		var audio1 = document.getElementsByTagName("audio")[0];
+		audio1.pause();
+		}
+function playAudio2(){
+        var audio2 = document.getElementsByTagName("audio")[1];
+        audio2.play();
+        }
+function stopAudio2(){
+		var audio2 = document.getElementsByTagName("audio")[1];
+		audio2.pause();
+		}
+function playAudio3(){
+        var audio3 = document.getElementsByTagName("audio")[2];
+        audio3.play();
+        }
+function stopAudio3(){
+		var audio3 = document.getElementsByTagName("audio")[2];
+		audio3.pause();
+		}
+function playAudio4(){
+        var audio4 = document.getElementsByTagName("audio")[3];
+        audio4.play();
+        }
+function stopAudio4(){
+		var audio4 = document.getElementsByTagName("audio")[3];
+		audio4.pause();
+		}
+function playAudio5(){
+        var audio5 = document.getElementsByTagName("audio")[4];
+        audio5.play();
+        }
+function stopAudio5(){
+		var audio5 = document.getElementsByTagName("audio")[4];
+		audio5.pause();
+		}
+function playAudio6(){
+        var audio6 = document.getElementsByTagName("audio")[5];
+        audio6.play();
+        }
+function stopAudio6(){
+		var audio6 = document.getElementsByTagName("audio")[5];
+		audio6.pause();
+		}
+function playAudio7(){
+        var audio7 = document.getElementsByTagName("audio")[6];
+        audio7.play();
+        }
+function stopAudio7(){
+		var audio7 = document.getElementsByTagName("audio")[6];
+		audio7.pause();
+		}
+function playAudio8(){
+        var audio8 = document.getElementsByTagName("audio")[7];
+        audio8.play();
+        }
+function stopAudio8(){
+		var audio8 = document.getElementsByTagName("audio")[7];
+		audio8.pause();
+		}
+function playAudio9(){
+        var audio9 = document.getElementsByTagName("audio")[8];
+        audio9.play();
+        }
+function stopAudio9(){
+		var audio9 = document.getElementsByTagName("audio")[8];
+		audio9.pause();
+		}
+function playAudio10(){
+        var audio10 = document.getElementsByTagName("audio")[9];
+        audio10.play();
+        }
+function stopAudio10(){
+		var audio10 = document.getElementsByTagName("audio")[9];
+		audio10.pause();
+		}
+function playAudio11(){
+        var audio11 = document.getElementsByTagName("audio")[10];
+        audio11.play();
+        }
+function stopAudio11(){
+		var audio11 = document.getElementsByTagName("audio")[10];
+		audio11.pause();
+		}
+function playAudio12(){
+        var audio12 = document.getElementsByTagName("audio")[11];
+        audio12.play();
+        }
+function stopAudio12(){
+		var audio12 = document.getElementsByTagName("audio")[11];
+		audio12.pause();
+		}
+function playAudio13(){
+        var audio13 = document.getElementsByTagName("audio")[12];
+        audio13.play();
+        }
+function stopAudio13(){
+		var audio13 = document.getElementsByTagName("audio")[12];
+		audio13.pause();
+		}
+function playAudio14(){
+        var audio14 = document.getElementsByTagName("audio")[13];
+        audio14.play();
+        }
+function stopAudio14(){
+		var audio14 = document.getElementsByTagName("audio")[13];
+		audio14.pause();
+		}
+function playAudio15(){
+        var audio15 = document.getElementsByTagName("audio")[14];
+        audio15.play();
+        }
+function stopAudio15(){
+		var audio15 = document.getElementsByTagName("audio")[14];
+		audio15.pause();
+		}
+function playAudio16(){
+        var audio16 = document.getElementsByTagName("audio")[15];
+        audio16.play();
+        }
+function stopAudio16(){
+		var audio16 = document.getElementsByTagName("audio")[15];
+		audio16.pause();
+		}
+function playAudio17(){
+        var audio17 = document.getElementsByTagName("audio")[16];
+        audio17.play();
+        }
+function stopAudio17(){
+		var audio17 = document.getElementsByTagName("audio")[16];
+		audio17.pause();
+		}
+function playAudio18(){
+        var audio18 = document.getElementsByTagName("audio")[17];
+        audio18.play();
+        }
+function stopAudio18(){
+		var audio18 = document.getElementsByTagName("audio")[17];
+		audio18.pause();
+		}
+function playAudio19(){
+        var audio19 = document.getElementsByTagName("audio")[18];
+        audio19.play();
+        }
+function stopAudio19(){
+		var audio19 = document.getElementsByTagName("audio")[18];
+		audio19.pause();
+		}
